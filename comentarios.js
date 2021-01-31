d66748add171c4860f998294a119f1183363a92d
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2021-01-31 19:06:23 -0300

    Added comments

---
+++ comentarios.js
@@ -0,0 +1,11 @@
+// Aqui eu tenho um comentário em linha
+
+/* 
+    Aqui eu tenho um comentário multi-linha
+
+    o código abaixo escreve alguma mensagem
+
+*/
+
+console.log('Bem vindos ao Starter')
+console.log('Não irá ignorar')
