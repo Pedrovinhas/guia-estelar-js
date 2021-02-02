ea4454b4bec1e62f4f6a7a97af6c8d4fe5503412
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2021-02-02 03:21:25 -0300

    Functions

--- functions/arguments_and_parameters.js
+++ functions/arguments_and_parameters.js
@@ -7,5 +7,21 @@ const sum = function(number1,number2) {
 }
 
 
+let number1 = 34
+let number2 = 25
 // Argumentos or arguments
 sum(2,3)
+
+
+const sum = function (number1, number2) {
+    total = number1 + number2
+    return total
+}
+
+let number1 = 34
+let number2 = 25
+
+console.log(`o número 1 é ${number1}`)
+console.log(`o número 1 é ${number2}`)
+console.log(`o número 1 é ${sum(number1, number2)}`)
+
---
+++ functions/ludic_function.js
@@ -0,0 +1,11 @@
+// Outra maneira de visualizar funções
+
+// Função é um liquidificador
+
+function fazerSuco(fruta1, fruta2) {
+    return 'suco de: ' + fruta1 + fruta2
+}
+
+const copo = fazerSuco('banana', 'maçã')
+
+console.log(copo)
