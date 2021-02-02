36c0bbf2637fd2aca154ee8ea5173a71bb4ab820
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2021-02-02 01:04:15 -0300

    Functions

---
+++ functions/arguments_and_parameters.js
@@ -0,0 +1,11 @@
+// Function expression
+// Anonymous function
+
+//Parâmetros or parameters
+const sum = function(number1,number2) {
+    console.log(number1 + number2)
+}
+
+
+// Argumentos or arguments
+sum(2,3)
