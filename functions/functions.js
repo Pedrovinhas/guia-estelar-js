c831b8826a5318fe13af791f809efe40b765325c
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2021-02-02 00:38:55 -0300

    Functions

---
+++ functions/functions.js
@@ -0,0 +1,15 @@
+// Criar aplicativo de frases motivacionais
+
+
+
+function createPhrases () {
+    console.log('Estudar é muito bom')
+    console.log('Paciência e persistência')
+    console.log('Revisão é mãe do aprendizado')
+}
+
+// Executar, rodar, chamar, invocar a função 
+// Execute, run, call, invoke a fuction
+createPhrases()
+
+console.log('fim do programa')
