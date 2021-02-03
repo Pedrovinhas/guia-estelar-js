ce8e6cc28252ba1976b7a3ad8aa8284739d3ac96
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2021-02-03 01:24:07 -0300

    Expressions and Operators

---
+++ expressions-and-operators/expressions_and_operators.js
@@ -0,0 +1,21 @@
+/* Expressões e Operadores
+
+ - Expressions
+ - Operators
+    Binary
+    Unary
+    Ternary
+*/
+
+// Expression
+let number = 1
+
+// Binary
+console.log (number + 3)
+
+// Unary
+console.log (++number)
+console.log (--number)
+
+// Ternary
+console.log(false ? 'alo' : 'nada')
