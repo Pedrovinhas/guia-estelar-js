3e6de32537ef008edf4a43ad5a74fce9a8188f77
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2021-02-04 01:20:19 -0300

    Operadores de Precedência

---
+++ expressions-and-operators/operators_precedence.js
@@ -0,0 +1,16 @@
+/*
+* grouping ()
+* negação e incremento              ! ++ --
+* multiplicação e divisão           * /     
+* adição e subtração                + -
+* relacional                        < <= > >=
+* igualdade                         == != === !==
+* AND                               &&
+* OR                                ││
+* condicional                       ?:
+* assignment (atribuição)           = += -= *=
+*/
+
+console.log ( 2 + 5 * 10) // 52
+console.log (ture == 1) // true
+console.log ( 3 > 2 && 2 > 1) // True
