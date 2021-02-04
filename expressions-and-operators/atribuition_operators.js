438fcbfcfb3fb8872c5dc7e79b8c1806517d4f87
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2021-02-04 01:10:49 -0300

    Operadores de Atribuição

---
+++ expressions-and-operators/atribuition_operators.js
@@ -0,0 +1,37 @@
+// Operadores de atribuição (Assignment)
+let x
+
+console.log(x)
+
+// Assignment
+x = 1
+
+// Addition Assignment
+// x = x + 2
+x += 2
+console.log(x)
+
+// Subtraction Assignment
+// x = x -2
+x -= 2
+console.log(x)
+
+// Multiplication Assignment
+// x = x * 2
+x *= 2
+console.log(x)
+
+// Division Assignment
+// x = x / 2
+x /= 2
+console.log(x)
+
+// Remainder 
+// x **=2
+x **=2
+console.log(x)
+
+// Exponetiation
+// x%=2
+x %= 2
+console.log(x)
