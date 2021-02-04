624c4f20a8ad785bbe5705a0986fac2b9b6f746a
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2021-02-04 01:11:32 -0300

    Operador Ternário

---
+++ expressions-and-operators/ternary_operator.js
@@ -0,0 +1,13 @@
+// Operador condicional ternário
+
+// Café da Manhã
+let pao = true
+let queijo = true
+
+const niceBreakfast = pao && queijo ? 'Café top' : 'Café ruim'
+console.log(niceBreakfast)
+
+// Maior de 18  
+let age = 16
+const canDrive = age >= 18 ? 'can drive' : "can't drive"
+console.log(canDrive)
