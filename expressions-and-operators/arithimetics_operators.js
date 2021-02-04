8c071d1af29d515cb036fbcefb172fda581ad1f4
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2021-02-04 01:10:10 -0300

    Operadores Aritméticos

---
+++ expressions-and-operators/arithimetics_operators.js
@@ -0,0 +1,31 @@
+// Operadores aritméticos
+
+// Multiplicação
+console.log(3.2*4.5)
+
+// Divisão
+console.log(12 / 4)
+
+// Soma
+console.log(34+23)
+
+// Subtração
+console.log(34-23)
+
+// Resto de divisão 
+let remainder
+remainder = 11 % 3
+console.log(remainder)
+
+// Increment++ e ++Increment
+let increment = 0
+console.log(++indecrement)
+console.log(increment++)
+
+// Decrement-- e --Decrement
+let decrement = 0
+console.log(--decrement)
+console.log(decrement--)
+
+// Exponencial
+console.log (3 ** 3)
