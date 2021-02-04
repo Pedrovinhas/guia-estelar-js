2466a2b55df9070292cb9205041348011ad7ce27
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2021-02-04 01:24:14 -0300

    Expressões e Operadores

--- expressions-and-operators/expressions_and_operators.js
+++ expressions-and-operators/expressions_and_operators.js
@@ -19,3 +19,25 @@ console.log (--number)
 
 // Ternary
 console.log(false ? 'alo' : 'nada')
+
+// New
+let names = new String ('Mayk')
+names.surName = 'Briot'
+let age = new Number(23)
+console.log(names,age)
+
+// New com Date
+let date = new Data('2020-12-01')
+console.log(date)
+console.log(data.__proto__)
+
+// typeof delete
+console.log(typeof "Mayk")
+
+const person = {
+    name: "Mayk",
+    age: 25,
+}
+delete person.age
+console.log(person)
+
