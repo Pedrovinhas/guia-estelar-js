293c878d56184263dc0a94991f13a281127ecc3d
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2021-02-04 01:17:01 -0300

    Operadores de Comparação

---
+++ expressions-and-operators/comparision_operators.js
@@ -0,0 +1,32 @@
+// Igual ==
+console.log(one == 2) // true
+
+console.log(one == "1") // true
+
+// Diferente !=
+console.log(one != two) // true
+console.log(one != 1) // false
+console.log(one != "1") // false
+
+// Estritamente igual ===
+console.log( one === "1") // false
+console.log( one === 1) // true
+
+// Estritamente diferente !==
+console.log( two === "2") // false
+console.log ( two === 2) // true
+
+// Maior >
+console.log(one > two) // false
+
+// Maior igual >=
+console.log(one >= 1) // true
+console.log(two >= 1) // true
+
+// Menor <
+console.log(one < two) // true
+
+// Menor igual <=
+console.log(one <= two) // true
+console.log(one <= 1) // true
+console.log(one <= 0) // true
